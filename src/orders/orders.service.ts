6eba6a0e871b2b770188f56a3b1060813f056106
Artag/Chris <scristxyz@gmail.com>
2024-11-19 13:28:25 -0500

    act pagination

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,7 +1,8 @@
-import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit, Query } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from '@prisma/client';
-import { RpcException } from '@nestjs/microservices';
+import { Payload, RpcException } from '@nestjs/microservices';
+import { OrderPaginationDto } from './dto/order-pagination.dto';
 
 
 @Injectable()
@@ -18,8 +19,29 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     return this.order.create({ data: createOrderDto });
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findAll(orderPaginationDto: OrderPaginationDto) {
+  const totalPages = await this.order.count({
+    where: {
+      status: orderPaginationDto.status
+  }})
+  const currentPage= orderPaginationDto.page || 1;
+  const perPage = orderPaginationDto.limit || 10;
+
+  return {
+    data:await this.order.findMany({
+      skip:(currentPage -1 )* perPage,
+      take: perPage,
+      where:{
+        status:orderPaginationDto.status
+      }
+    }),
+    meta:{
+      total:totalPages,
+      page:currentPage,
+      lastPage: Math.ceil(totalPages/perPage)
+    }
+  } 
+
   }
 
   async findOne(id: string) {
